7a0d0855b31fcc5c5b61ba2923462a3b2b467965
solgitrepo <solanalyticss@gmail.com>
2025-06-26 11:55:04 +0400

    Create __init__.py

---
+++ database/__init__.py
@@ -0,0 +1,8 @@
+"""
+Database package for Customer Feedback Form application.
+"""
+
+# Database package for Customer Feedback Form
+# This file makes the 'database' directory a Python package
+
+__all__ = ['connection', 'models', 'operations'] 
